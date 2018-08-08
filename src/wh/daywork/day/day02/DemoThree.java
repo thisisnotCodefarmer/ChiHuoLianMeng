aed0ac96fae051ebd902f4b6d75d94a15007351f
thisisnotCodefarmer <1043405049@qq.com>
2018-08-08 16:44:46 +0800

    test1.3

---
+++ src/wh/daywork/day/day02/DemoThree.java
@@ -0,0 +1,10 @@
+package wh.daywork.day.day02;
+
+public class DemoThree {
+	public static void main(String[] args) {
+		final String NAME = "金三胖";
+		final String JOB = "领导人";
+		final String LOVE = "核武器";
+		final String COUNTRY = "朝鲜";
+	}
+}
